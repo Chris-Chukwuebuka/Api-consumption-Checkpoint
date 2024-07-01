601d5679050325eabbef2b3d516ece87927f1b89
Ebuka-Chucks <chrisbrandford4@gmail.com>
2024-07-01 21:31:47 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Layout from "./Layout/Layout";
+import "./App.css"
+
+const App = () => {
+
+  return (
+    <div className="App-header">
+    <Layout />
+
+    </div>
+  );
+};
+
+export default App;
